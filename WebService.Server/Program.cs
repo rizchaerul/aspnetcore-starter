27bb761ad002c7b1ef14c72cfa1c310cf8e9abdd
Chaerul Rizky <riz.chaerul@gmail.com>
2023-12-30 00:34:58 +0700

    add controller support

--- WebService.Server/Program.cs
+++ WebService.Server/Program.cs
@@ -1,21 +1,28 @@
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+// Add services to the container
+services.AddControllers();
+services.AddOpenApiDocument();
+services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi3();
+}
+
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
+app.MapControllers();
 app.MapRazorPages();
-
 app.Run();
